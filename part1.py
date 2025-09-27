b41837284e46eb5432eb778eb24713962faed52c
Marc Haye <marc.antonio.haye@gmail.com>
2025-09-27 20:19:22 +0200

    feat: reponses partie 1

--- part1.py
+++ part1.py
@@ -1,20 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+"""
+Questions
+
+1)
+x=xc+r*cos(teta)
+y=yc+r*sin(teta)
+
+BX=np.linspace(xc,x,10)
+BY=np.linspace(yc,y,10)
+
+2) teta=np.linspace(0,2*np.pi,100)
+
+"""
+
 # Centre et rayon du cercle
-xc,yc=1,1
+Xc,Yc=1,1
 r=2
 
 # Définition de téta, définie sur [0,2pi[
 teta=np.linspace(0,2*np.pi,100)
 
 # Système des points du cercle
-x=xc+r*np.cos(teta)
-y=yc+r*np.sin(teta)
+x=Xc+r*np.cos(teta)
+y=Yc+r*np.sin(teta)
 
 #Tableaux d'abscisses et d'ordonnées
-BX=np.linspace(xc,x,10)
-BY=np.linspace(yc,y,10)
+BX=np.linspace(Xc,x,10)
+BY=np.linspace(Yc,y,10)
 
 # Nuage de points
 l1=plt.plot(BX,BY)
